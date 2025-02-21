8fff4d9cae909c460c16e44b0f3ace7ba2919f69
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 11:14:07 +0800

    :sparkles: feat: Add useState cart and setCart in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import HomePage from "./pages/HomePage.jsx";
 import ShopPage from "./pages/ShopPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <div className="App">
       <HashRouter>
-        <Navbar />
+        <Navbar cartCount={cart.length} />
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<HomePage />} />
