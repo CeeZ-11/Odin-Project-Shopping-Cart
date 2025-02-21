09cf3dfddc44eda22c13f3d079fc3eb897751cee
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 10:25:14 +0800

    :sparkles: feat: Add Navbar component in components folder and import to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HomePage />
     </div>
   );
