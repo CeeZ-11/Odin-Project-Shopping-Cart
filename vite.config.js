4611629819a70d3d3faf502fde8953824a3577a2
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 22:02:52 +0800

    :wrench: chore: remove critters

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Critters from "critters";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,34 +8,6 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  plugins: [
-    react(),
-    {
-      name: "vite-plugin-critters",
-      apply: "build",
-      enforce: "post",
-      async closeBundle() {
-        const { readFile, writeFile } = await import("fs/promises");
-        const path = "./dist/index.html";
-        try {
-          let html = await readFile(path, "utf8");
-          const critters = new Critters({
-            preload: "swap",
-            inlineFonts: true,
-            pruneSource: true,
-          });
-          html = await critters.process(html);
-          await writeFile(path, html, "utf8");
-          console.log("✅ Critters applied: Critical CSS inlined!");
-        } catch (error) {
-          console.error("❌ Critters processing failed:", error);
-        }
-      },
-    },
-  ],
-  build: {
-    cssCodeSplit: true,
-    outDir: "dist",
-  },
+  plugins: [react()],
   base: "/Odin-Project-Shopping-Cart/",
 });
