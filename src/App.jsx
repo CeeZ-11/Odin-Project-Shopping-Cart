c40ee456ea72aafa9d3468ee8aa0867081f4db6e
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 10:10:10 +0800

    :sparkles: feat: Create components and pages folder and Add HomePage component in pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Shopping Cart App</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Welcome to the Shopping Cart</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
