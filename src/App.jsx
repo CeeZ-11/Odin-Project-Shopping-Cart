466d3cb7f192f416a941068165747a3e9adc26c0
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 10:55:09 +0800

    :sparkles: feat: Add HashRouter, Route, Routes, and Suspense from react router dom to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
 import Navbar from "./components/Navbar.jsx";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <HomePage />
+      <HashRouter>
+        <Navbar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Suspense>
+      </HashRouter>
     </div>
   );
 }
