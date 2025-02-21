65d34a82c9ce4fbaa00028c85c30376ea8f50e51
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 12:11:23 +0800

    :sparkles: feat: Add server live in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react()],
   base: "/Odin-Project-Shopping-Cart/",
 });
